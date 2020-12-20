5819e4f9af8e1998be9fdbcdcfecfe779bfde49d
Nikita <dr_mario@ua.fm>
2020-12-20 07:05:08 +0200

    initial commit

--- ATM.java
+++ ATM.java
@@ -1,7 +1,6 @@
 public class ATM {
         public int countBanknotes(int sum){
             int[] arr = {500, 200, 100, 50, 20, 10, 5, 2, 1};
-
             int count = 0;
             int a = 0;
             int b = sum;
